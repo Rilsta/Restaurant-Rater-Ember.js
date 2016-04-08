206d4d6ad4444bdd6b09cfb0cd25764782eebd00
rilsta <rstarnes9@gmail.com>
2016-04-08 13:56:17 -0700

    add service to add restaruants to a list of favorites

--- app/components/restaurant-detail.js
+++ app/components/restaurant-detail.js
@@ -1,12 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteRestaurants: Ember.inject.service(),
   actions: {
     update(comment, params){
       this.sendAction('update', comment, params);
     },
     destroyComment(comment) {
       this.sendAction('destroyComment', comment)
-    }
+    },
+    addToFavorites(favorite) {
+      this.get('favoriteRestaurants').add(favorite);
+    },
   }
 });
