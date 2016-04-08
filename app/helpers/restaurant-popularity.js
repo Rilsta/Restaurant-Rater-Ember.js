26c7c4c8f7a97a6ebc0e5babcf8bd7ea25ab7345
rilsta <rstarnes9@gmail.com>
2016-04-08 14:11:44 -0700

    add helper to show when a restaurant has comments

---
+++ app/helpers/restaurant-popularity.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function restaurantPopularity(params/*, hash*/) {
+  var restaurant = params[0];
+
+  if(restaurant.get('comments').get('length') >= 1 ) {
+    return Ember.String.htmlSafe('<i class="fa fa-comments-o"></i>')
+  }
+}
+
+export default Ember.Helper.helper(restaurantPopularity);
---
+++ tests/unit/helpers/restaurant-popularity-test.js
@@ -0,0 +1,10 @@
+import { restaurantPopularity } from 'restaurant-rater/helpers/restaurant-popularity';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | restaurant popularity');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = restaurantPopularity([42]);
+  assert.ok(result);
+});
