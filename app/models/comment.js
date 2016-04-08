16ae72f6899ec5fe910f5084a53f1bda765e5711
rilsta <rstarnes9@gmail.com>
2016-04-08 08:38:23 -0700

    add restaurant and comment model information

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  userName: DS.attr(),
+  rating: DS.attr(),
+  description: DS.attr(),
+  restaurant: belongsTo('restaurant', {async: true})
 });
--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  restaurantName: DS.attr(),
+  address: DS.attr(),
+  phone: DS.attr(),
+  image: DS.attr(),
+  comments: hasMany('comment', {async: true})
 });
