cc3c983eb68cbee3f54c14b7844fb398a3657a24
rilsta <rstarnes9@gmail.com>
2016-04-08 10:07:34 -0700

    add save action to new-comment

--- app/components/comment-tile.js
+++ app/components/comment-tile.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewComment: false,
+  actions: {
+    commentFormShow() {
+      this.set('addNewComment', true);
+    },
+    save(restaurant) {
+      var params = {
+        userName: this.get('userName'),
+        description: this.get('description'),
+        rating: this.get('rating')
+      };
+      this.set('addNewComment', false);
+      this.sendAction('save', params);
+    },
+  }
 });
