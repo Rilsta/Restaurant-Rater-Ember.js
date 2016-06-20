48058a255a6cca516e1c83b0f272f5d2543a65e5
rilsta <rstarnes9@gmail.com>
2016-06-20 10:51:30 -0700

    work on getting favorites to live update on main page

--- app/components/restaurant-tile.js
+++ app/components/restaurant-tile.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteRestaurants: Ember.inject.service(),
+  actions: {
+    addToFavorites(favorite) {
+      this.get('favoriteRestaurants').add(favorite);
+    },
+  }
 });
